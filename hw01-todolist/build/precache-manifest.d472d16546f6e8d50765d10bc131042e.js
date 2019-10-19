a1917b36bab34a243276955ee51b8cef5e17333c
zhengshangzi <1643613240@qq.com>
2019-10-19 15:41:08 +0800

    zsz

---
+++ hw01-todolist/build/precache-manifest.d472d16546f6e8d50765d10bc131042e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b92b91a28f1300b3f7e44c98c23131d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8485222b2aba411f461e",
+    "url": "/static/css/main.54bf3d9c.chunk.css"
+  },
+  {
+    "revision": "91bff1f08c9c4326bd74",
+    "url": "/static/js/2.9e898f0c.chunk.js"
+  },
+  {
+    "revision": "8485222b2aba411f461e",
+    "url": "/static/js/main.87fa1221.chunk.js"
+  },
+  {
+    "revision": "d6bdb167f23a9916b64c",
+    "url": "/static/js/runtime-main.d2bdd3d6.js"
+  }
+]);
